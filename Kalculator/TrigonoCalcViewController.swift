cb2d272a9a80eae69985c4ba4c24b0888f12b3af
imjog <imjog24@gmail.com>
2017-07-12 18:02:42 +0530

    Trigonometric calculation controller added

---
+++ Kalculator/TrigonoCalcViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrigonoCalcViewController.swift
+//  Kalculator
+//
+//  Created by JOGENDRA on 12/07/17.
+//  Copyright © 2017 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class TrigonoCalcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backToMain(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+}
