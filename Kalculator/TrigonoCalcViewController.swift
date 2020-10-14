fe5303bc287ba886ec82e6c0be14a8325b63ed66
unparalleled-js <yingthi@live.com>
2020-10-14 11:46:39 -0500

    WIP

--- Kalculator/TrigonoCalcViewController.swift
+++ Kalculator/TrigonoCalcViewController.swift
@@ -12,14 +12,10 @@ class TrigonoCalcViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
 }
